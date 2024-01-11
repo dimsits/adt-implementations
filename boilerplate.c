76c0a082c75317c1e88690cc3f6bd32888b5b6f8
DIMSITS <sethdemeterio47@gmail.com>
2024-01-11 15:18:30 +0800

    Boilerplate addition

---
+++ boilerplate.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+//STRUCTURE DEFINITIONS
+typedef struct {
+    char fName[10];
+    char lName[20];
+    char mInitial;
+    int age;
+}person;
+
+//FUNCTION HEADERS
+
+void main() {
+
+}
+
+//FUNCTION DEFINITIONS
