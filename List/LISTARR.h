519caa05342ceed2972ce21b48e183139bcc28c9
DIMSITS <sethdemeterio47@gmail.com>
2024-03-08 20:35:10 +0800

    Practices

--- List/LISTARR.h
+++ List/LISTARR.h
@@ -9,7 +9,6 @@ typedef struct {
 typedef enum {TRUE, FALSE}Bool;
 
 void initList(ListPTR *A);
-void expandList(ListPTR *A);
 void insertPos(ListPTR *A, char elem, int pos);
 void deleteElem(ListPTR *A, char elem);
 Bool findElem(ListPTR A, char elem);
@@ -21,9 +20,20 @@ void initList(ListPTR *A) {
     *A->count = 0;
 }
 
-void insertPos(ListPTR *A, char elem, int pos) {
-    *A->count % SIZE == 0 && *A->count > 0  ? realloc(&(*A), (*A->count + SIZE));
+void insertPos(ListPTR A, char elem, int pos) {
+    A->count % SIZE == 0 && *A->count > 0  ? realloc(&(*A), (*A->count + SIZE));
+    int i;
+    for (i = A->count - 1; i > pos; i--) {
+        A->elem[i + 1] = A->elem[i];
+    }
+    A->elem[i] = elem;
+}
 
+void displayList(ListPTR A) {
+    int i;
+    for (i = 0; i < A->count; i++) {
+        
+    }
 }
 
 #endif
