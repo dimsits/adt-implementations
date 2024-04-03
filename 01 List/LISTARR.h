0e4a8a500f962109d27cdb197f6cc221d6b70f4c
DIMSITS <sethdemeterio47@gmail.com>
2024-04-03 21:33:37 +0800

    Files

--- 01 List/LISTARR.h
+++ 01 List/LISTARR.h
@@ -1,40 +1,39 @@
 #ifndef LISTARR.h
 #define LISTARR.h
 #define SIZE 10
-typedef struct {
+
+typedef struct LType{
     char *elem;
     int count;
-}List, *ListPTR;
+}*List;
 
-typedef enum {TRUE, FALSE}Bool;
+typedef enum {
+    TRUE, FALSE'
+}Bool;
 
-void initList(ListPTR *A);
-void insertPos(ListPTR *A, char elem, int pos);
-void deleteElem(ListPTR *A, char elem);
-Bool findElem(ListPTR A, char elem);
-void displayList(ListPTR A);
+void initList(List *A);
+Bool findElem(List A);
+void insertSorted(List A, int elem);
+void deleteElem(List A, int elem);
+void displayList(List A);
 
-void initList(ListPTR *A) {
-    *A = (ListPTR)malloc(sizeof(List));
-    *A->elem = (char*)malloc(SIZE * sizeof(char));
-    *A->count = 0;
-}
-
-void insertPos(ListPTR A, char elem, int pos) {
-    A->count % SIZE == 0 && *A->count > 0  ? realloc(&(*A), (*A->count + SIZE));
-    int i;
-    for (i = A->count - 1; i > pos; i--) {
-        A->elem[i + 1] = A->elem[i];
-    }
-    A->elem[i] = elem;
-}
-
-void displayList(ListPTR A) {
-    int i;
-    for (i = 0; i < A->count; i++) {
-        
+void initList(List *A) {
+    *A = (List)malloc(sizeof(LType));
+    if ((*A) != NULL) {
+        (*A)->count = 0;
+        (*A)->elem = (char*)malloc(SIZE * sizeof(char));
     }
 }
+
+Bool findElem(List A) {
+	Bool ret = FALSE;
+	int i;
+	for (i = 0; i < A->count; i++) {
+		
+	}
+}
+
+
 
 #endif
 
--- 01 List/main.c
+++ 01 List/main.c
@@ -1,5 +1,11 @@
 #include "../includes.h"
+#include "LISTARR.h"
 
 void main() {
-
+    ListPTR L;
+    initList(&L);
+    insertSorted(L, 4);
+    insertSorted(L, 3);
+    insertSorted(L, 6);
+    displayList(L);
 }
